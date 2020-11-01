afad953c19235e8f510dec0591b03cf114ab81b3
Karlinator <kehofset@live.no>
2020-11-01 15:58:50 +0100

    Cart structure and number field.

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import {TableCell, Table, TableContainer, TableHead, TableRow, TableBody} from "@material-ui/core";
+import {
+    TableCell,
+    Table,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TableBody,
+    Container,
+    AppBar,
+    Toolbar,
+    IconButton,
+    TextField, Button,
+} from "@material-ui/core";
+import {ArrowBack} from "@material-ui/icons";
 
 const useStyles = makeStyles({
     table: {
@@ -8,6 +21,9 @@ const useStyles = makeStyles({
     },
     img: {
         maxHeight: 100,
+    },
+    number: {
+        width: 70,
     }
 });
 
@@ -44,6 +60,24 @@ const products = [
 
 
 const Cart = () => {
+    return (
+        <div>
+            <AppBar position="sticky">
+                <Toolbar>
+                    <IconButton color="inherit" edge="start" aria-label="tilbake">
+                        <ArrowBack />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Container>
+                <CartList products={products} shipping={149} />
+                <Button variant="contained" color="primary">Kjøp</Button>
+            </Container>
+        </div>
+    )
+}
+
+const CartList = (props) => {
     const classes = useStyles();
     return (
         <TableContainer>
@@ -58,16 +92,33 @@ const Cart = () => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {products.map((row) => (
+                    {props.products.map((row) => (
                         <TableRow>
                             <TableCell align="center"><img alt="" className={classes.img} src={row.image}/></TableCell>
                             <TableCell align="center">{row.name}</TableCell>
                             <TableCell align="center">{row.variant}</TableCell>
-                            <TableCell align="center">{row.quantity}</TableCell>
+                            <TableCell align="center">
+                                <TextField
+                                    className={classes.number}
+                                    variant="outlined"
+                                    type="number"
+                                    size="small"
+                                    value={row.quantity}
+                                />
+                            </TableCell>
                             <TableCell align="center">{row.price}</TableCell>
                             <TableCell align="center">{row.quantity*row.price}</TableCell>
                         </TableRow>
                     ))}
+                    <TableRow>
+                        <TableCell rowSpan={3} colSpan={4} />
+                        <TableCell align="center">Frakt:</TableCell>
+                        <TableCell align="center">{props.shipping}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell align="center">Total:</TableCell>
+                        <TableCell align="center">{props.products.reduce((sum, p) => sum + p.quantity*p.price, 0)}</TableCell>
+                    </TableRow>
                 </TableBody>
             </Table>
         </TableContainer>
