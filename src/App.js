16672aa29f02afe6677a0d97cad495a221ad469b
Karlinator <kehofset@live.no>
2020-11-06 06:19:02 +0100

    Delete old price based keys in localstorage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Browse from "./browse/Browse";
 import Admin from "./admin/Admin";
 
 function App() {
-    if (!window.localStorage.getItem('cart') /*|| JSON.parse(window.localStorage.getItem('cart')).find(v => v.id.includes('prod'))*/) {
+    if (!window.localStorage.getItem('cart') || JSON.parse(window.localStorage.getItem('cart')).find(v => v.id.includes('price'))) {
         window.localStorage.setItem('cart', '[]')
     }
     return (
